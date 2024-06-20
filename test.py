42d7fae12bd20ba0e94a6f058f81798b35edcf1f
MinUkiki <77089278+MinUkiki@users.noreply.github.com>
2024-06-20 19:26:21 +0900

    수정

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import gym
+import gymnasium as gym
 env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset(seed=42)
 for _ in range(1000):
-   action = policy(observation)  # User-defined policy function
+   action = env.action_space.sample()
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
